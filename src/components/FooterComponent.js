9cf089b6cb7c77f5d5790ee52288fd0ee41cd5b3
bachar-alhamada <bachar.alhamada76@gmail.com>
2024-05-31 15:05:46 -0500

    mm

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -5,15 +5,15 @@ function FooterComponent() {
       <div className="content-box">
         <p className="flex gap-20 center">
           <span>&copy; 2024</span>
-          <a href="https://bachar-alhamada.com" target="_blank" rel="noreferrer">
+          <a href="https://github.com/bachar157/react-project.git" target="_blank" rel="noreferrer">
             Bachar Alhamada
           </a>
           <span>|</span>
-          <a href="https://linkedin.com/in/bacharmalhamada" target="_blank" rel="noreferrer">
+          <a href="https://github.com/bachar157/react-project.git" target="_blank" rel="noreferrer">
             LinkedIn
           </a>
           <span>|</span>
-          <a href="https://github.com/bacharmalhamada" target="_blank" rel="noreferrer">
+          <a href="https://github.com/bachar157/react-project.git" target="_blank" rel="noreferrer">
             GitHub
           </a>
         </p>
